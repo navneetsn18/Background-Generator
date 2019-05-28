468e172f945c6ef36ad008ca164bdc287efb6897
Navneet Singh Negi <33743031+navneetsn18@users.noreply.github.com>
2019-05-29 01:42:33 +0530

    Add files via upload

---
+++ cssgred.js
@@ -0,0 +1,22 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradientColor(){
+	body.style.background = "linear-gradient(to right,"
+							+
+							color1.value
+							+
+							","
+							+
+							color2.value
+							+")"
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input",setGradientColor);
+
+color2.addEventListener("input",setGradientColor);
+
